70ca027f1b9af74f9714ba4b7ad5bd3361004f53
curegit <37978051+curegit@users.noreply.github.com>
2025-11-09 23:17:11 +0900

    Fix

--- install.ps1
+++ install.ps1
@@ -1,15 +1,17 @@
 #!/usr/bin/env pwsh
 
+using namespace System.Management.Automation
+
 $documentsPath = [Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
-$customMapPath = Join-Path $documentsPath "My Games/Beyond the Sword(J)/PublicMaps"
-if (-Not (Test-Path $customMapPath)) {
-  $customMapPath = Join-Path $documentsPath "My Games/Beyond the Sword/PublicMaps"
+$customMapPath = Join-Path -Path ([WildcardPattern]::Escape($documentsPath)) -ChildPath "My Games/Beyond the Sword(J)/PublicMaps"
+if (-Not (Test-Path -LiteralPath $customMapPath)) {
+  $customMapPath = Join-Path -Path ([WildcardPattern]::Escape($documentsPath)) -ChildPath "My Games/Beyond the Sword/PublicMaps"
 }
-if (-Not (Test-Path $customMapPath)) {
+if (-Not (Test-Path -LiteralPath $customMapPath)) {
   throw "The PublicMaps path does not exist."
 }
 Write-Output $customMapPath
 
 $scriptPath = $PSScriptRoot
-$publicMapsPath = Join-Path $scriptPath "PublicMaps"
-Copy-Item "$publicMapsPath/*" $customMapPath -Recurse -Verbose
+$publicMapsPath = Join-Path -Path ([WildcardPattern]::Escape($scriptPath)) -ChildPath "PublicMaps"
+Copy-Item -Path (([WildcardPattern]::Escape($publicMapsPath)) + "/*") -Destination $customMapPath -Recurse -Verbose
